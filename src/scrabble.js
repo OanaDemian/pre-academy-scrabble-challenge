77cde91f091001b97189006a73514a6977989b7e
Oana <oana.demian@gmail.com>
2023-05-27 15:03:36 +0100

    calculates the score with word multiplier

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,10 +1,10 @@
 import {scoringPoints} from "./scoringPoints.js";
 import {scrabbleGrid} from "./scrabbleGrid.js";
-class Scrabble {
 
+export class Scrabble {
   constructor(word, position, direction) {
-    const regex = /[0-9$/\\&+,:;=?@#|'<>.^*()%!-]/;
-    if (regex.test(word)){
+    const scrabbleTilesRegex = /[0-9$/\\&+,:;=?@#|'<>.^*()%!-]/;
+    if (scrabbleTilesRegex.test(word)) {
       throw new Error ('Words can only contain letters.');
     } else { 
        this.word = word;
@@ -12,67 +12,76 @@ class Scrabble {
     this.position = position;
     this.direction = direction;
   }
-//returns the score for the word
+
+  //returns the score for the word
   score() {
     if (this.isEmptyWord() || this.isNull() || this.isWhiteSpace()) return 0;
-    return this.calculateScore();
+    const baseScore = this.calculateBaseScore();
+    const wordMultipliers = this.calculateWordMultipliers();
+    const finalScore = wordMultipliers.reduce((finalScore, multiplier) => finalScore * multiplier, baseScore)
+    return finalScore;
   }
-//calculates the score for the word and returns it
-  calculateScore() {
-    const score = this.wordLetters().reduce((acc, letter) => 
-      letter in scoringPoints ? acc += parseInt(scoringPoints[letter]) * this.wordMultiplier(letter) * this.letterMultiplier(letter) : acc, 
-      0
-    );
-    return score;
-  }
-// calculates letter multiplier
 
+  //calculates the base score for the word without word multipliers
+  calculateBaseScore = () => this.wordLetters().reduce((acc, letter) => 
+    letter in scoringPoints ? acc += this.calculateTileScore(letter) : acc, 
+    0
+  );
+
+  //calculates the base score for the tile with letter multipliers
+  calculateTileScore = (letter) => parseInt(scoringPoints[letter]) * this.letterMultiplier(letter);
+
+  // gets word multiplier value for each tile
+  calculateWordMultipliers = () => this.wordLetters().map((letter) => this.wordMultiplier(letter));
+
+  // calculates letter multiplier
   letterMultiplier(letter) {
-    const letterPosition = this.lettersAndPositions().filter((letterPosition) => (letterPosition.letter === letter))[0].position;
+    const letterPosition = this.lettersAndPositions().filter((letterPosition) => (
+      letterPosition.letter === letter))[0].position;
     const letterPositionMultiplier = this.filterByPosition(letterPosition).letterMultipler;
     return letterPositionMultiplier;
   }
-// calculates word multiplier
+
+  // calculates word multiplier
   wordMultiplier(letter) {
-    const letterPosition = this.lettersAndPositions().filter((letterPosition) => (letterPosition.letter === letter))[0].position;
+    const letterPosition = this.lettersAndPositions().filter((letterPosition) => (
+      letterPosition.letter === letter))[0].position;
     const wordPositionMultiplier = this.filterByPosition(letterPosition).wordMultiplier;
     return wordPositionMultiplier;
   }
 
-  //filters the word letters coordinates to find a word multiplyier different than 1
+  //returns an array of word letters
+  wordLetters = () => this.word.split("").map((letter) => letter.toLowerCase())
 
-  filterWordForPremiumWordTile() {
-    let score = 1;
-    this.wordLetters().filter((letter) => score *= this.wordMultiplier(letter))
-    return score;
-  }
-
-//returns an array of word letters
-  wordLetters() {
-    const letters = this.word.split("").map((letter) => letter.toLowerCase());
-    return letters;
-  }
-//returns an object containing each word letter and its position on the scrabble grid 
-  lettersAndPositions() {
-    const lettersAndPositions = this.wordLetters().map((letter,index) => ({
+  //returns an object containing each word letter and its position on the scrabble grid 
+  lettersAndPositions = () => this.wordLetters().map((letter, index) => (
+    {
       letter: letter.toLowerCase(),
       position: this.positionByIndex(index)
-    }));
-    return lettersAndPositions;
-  }
-//returns the grid coordinates for each letter in the word
+    }
+  ));
+
+  //returns the grid coordinates for each letter in the word
   positionByIndex(index) {
-    let newVal = 0;
-    let posByIndex = {} ;
-    if (this.direction === 'horizontal') {newVal = this.position['x'] + index; posByIndex = {x: newVal, y: this.position['y']}};
-    if (this.direction === 'vertical') {newVal = this.position['y'] - index; posByIndex = {x: this.position['x'], y: newVal}}; 
-    return posByIndex;
+    if (this.direction === 'horizontal') {
+      const tileOffset = this.position['x'] + index;
+      return { x: tileOffset, y: this.position['y'] }
+    } else {
+      const tileOffset = this.position['y'] - index;
+      return { x: this.position['x'], y: tileOffset }
+    }
   }
-// filters the scrabble board object for the tile with the corresponding coordinates. Returns an object contaning {position, letterMultiplier and wordMultiplier}
+
+  // filters the scrabble board object for the tile with the corresponding coordinates.
+  // Returns an object contaning {position, letterMultiplier and wordMultiplier}
   filterByPosition(position) {
-    const coordinateInGridTile = scrabbleGrid.filter(tile => (tile.position['x'] === position['x']) && (tile.position['y'] === position['y']));
-    return coordinateInGridTile[0];
+    const coordinateInGridTile = scrabbleGrid.find(tile => (
+      tile.position['x'] === position['x'] && 
+      tile.position['y'] === position['y']
+    ));
+    return coordinateInGridTile;
   }
+
   isEmptyWord = () => (this.word === "" ? true : false);
 
   isNull = () => (this.word === null ? true : false);
@@ -80,7 +89,6 @@ class Scrabble {
   isWhiteSpace = () => (" \t\n\r\v".indexOf(this.word) > -1 ? true : false);
 }
 
-export default Scrabble;
 const scrabble = new Scrabble('chair', {x: 0 , y: 14}, 'horizontal');
 console.log(scrabble.score())
 
